6c57cba8d2fc40e6fc269483a427506bdf0bb3e2
Ariel Maniago <admaniago01@gmail.com>
2025-10-26 12:08:22 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ayiezac/golang-calculator
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	appName := "Calculator"
+
+	fmt.Printf("%s", appName)
+}
