f2a4d822d26ca139b61c1e617f6692ed43cdbb35
Ariel Maniago <admaniago01@gmail.com>
2025-10-26 13:26:10 +0800

    basic calculator

--- main.go
+++ main.go
@@ -4,8 +4,43 @@ import (
 	"fmt"
 )
 
-func main(){
+func main() {
+
+	var operation string
+	var firstNum int
+	var secondNum int
 	appName := "Calculator"
 
-	fmt.Printf("%s", appName)
+	fmt.Printf("%s\n", appName)
+	fmt.Println("Please choose operation: (+, -, *, /)")
+	fmt.Scanln(&operation)
+
+	switch operation {
+	case "+":
+		fmt.Println("Input first number")
+		fmt.Scanln(&firstNum)
+		fmt.Println("Input second number")
+		fmt.Scanln(&secondNum)
+		fmt.Println(firstNum + secondNum)
+	case "-":
+		fmt.Println("Input first number")
+		fmt.Scanln(&firstNum)
+		fmt.Println("Input second number")
+		fmt.Scanln(&secondNum)
+		fmt.Println(firstNum - secondNum)
+	case "*":
+		fmt.Println("Input first number")
+		fmt.Scanln(&firstNum)
+		fmt.Println("Input second number")
+		fmt.Scanln(&secondNum)
+		fmt.Println(firstNum * secondNum)
+	case "/":
+		fmt.Println("Input first number")
+		fmt.Scanln(&firstNum)
+		fmt.Println("Input second number")
+		fmt.Scanln(&secondNum)
+		fmt.Println(float32(firstNum) / float32(secondNum))
+	default:
+		fmt.Println("Invalid operation")
+	}
 }
