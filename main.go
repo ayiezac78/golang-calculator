4f2d7b5b710f4b485e68868514f339a0acf31682
Ariel Maniago <admaniago01@gmail.com>
2026-01-28 20:00:21 +0800

    chore: defined structs and interface

--- main.go
+++ main.go
@@ -9,17 +9,18 @@ import (
 
 func main() {
 	var firstNum, secondNum int
-	anotherOperation := bool(true)
+	var continueOperation = bool(false)
+	var inputOperation string = "Please choose operation: (+, -, *, /): "
 	calc := calculator.Calculations{
 		Num1: firstNum,
 		Num2: secondNum,
 	}
 
-	appName := "Calculator"
+	appName := "Welcome to Azriel's Calculator"
 
 	fmt.Printf("%s\n", appName)
 
-	fmt.Print("Please choose operation: (+, -, *, /): ")
+	fmt.Print(inputOperation)
 ops:
 	for {
 		var op string
@@ -30,12 +31,20 @@ ops:
 			// calc.Num1 = firstNum
 			// calc.Num2 = secondNum
 			fmt.Println("Result:", calc.Sum())
+
+			if !continueOperation {
+				var response string
+				wantsAnotherOperation(response, inputOperation)
+				continue
+			}
+
 			break ops
 		case "-":
 			inputNum(&calc.Num1, &calc.Num2)
 			// calc.Num1 = firstNum
 			// calc.Num2 = secondNum
 			fmt.Println("Result:", calc.Subtract())
+
 			break ops
 		case "*":
 			inputNum(&calc.Num1, &calc.Num2)
@@ -57,25 +66,13 @@ ops:
 				}
 				fmt.Println("Result:", result)
 
-				// ask for another operation
-				fmt.Print("Do you want to perform another operation? (y/n): ")
-				var another string
-				fmt.Scanln(&another)
-				if another == "y" || another == "Y" {
-					anotherOperation = true
-				}
-
-				if anotherOperation {
-					fmt.Print("Please choose operation: (+, -, *, /): ")
-					continue ops
-				}
-
 				break ops
 			}
 		default:
 			fmt.Println("Invalid operation")
 			break ops
 		}
+
 	}
 }
 
@@ -84,4 +81,14 @@ func inputNum(a, b *int) {
 	fmt.Scanln(a)
 	fmt.Print("Input second number: ")
 	fmt.Scanln(b)
+}
+
+func wantsAnotherOperation(response, inputOperation string) bool {
+	fmt.Print("Do you want to perform another operation? (y/n): ")
+	fmt.Scanln(&response)
+	if response == "y" {
+		fmt.Print(inputOperation)
+		return true
+	}
+	return false
 }
